71c29bd18dc769588df5895b89e18481f8c0e094
kumarabm <kumarabm11@gmail.com>
2025-05-05 12:07:21 +0530

    modify url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // backend base URL
+  baseURL: "https://sales-bill-backend-sqz9.vercel.app//api", // backend base URL
 });
 
 export default api;
